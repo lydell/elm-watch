77342454dd9951827b6b351673692b27ae2c633e
Simon Lydell <simon.lydell@gmail.com>
2021-05-23 15:57:14 +0200

    Even smaller uglify output

--- example/postprocess.js
+++ example/postprocess.js
@@ -3,6 +3,25 @@ const UglifyJs = require("uglify-js");
 
 const [outputPath, mode] = process.argv.slice(2);
 
+const pureFuncs = [
+  "F2",
+  "F3",
+  "F4",
+  "F5",
+  "F6",
+  "F7",
+  "F8",
+  "F9",
+  "A2",
+  "A3",
+  "A4",
+  "A5",
+  "A6",
+  "A7",
+  "A8",
+  "A9",
+]
+
 switch (mode) {
   case "standard":
   case "debug":
@@ -11,50 +30,26 @@ switch (mode) {
   case "optimize": {
     const code = fs.readFileSync(outputPath, "utf8");
 
-    const result1 = UglifyJs.minify(code, {
+    const result = UglifyJs.minify(code, {
       compress: {
-        pure_funcs: [
-          "F2",
-          "F3",
-          "F4",
-          "F5",
-          "F6",
-          "F7",
-          "F8",
-          "F9",
-          "A2",
-          "A3",
-          "A4",
-          "A5",
-          "A6",
-          "A7",
-          "A8",
-          "A9",
-        ],
+        pure_funcs: pureFuncs,
         pure_getters: true,
         keep_fargs: false,
         unsafe_comps: true,
         unsafe: true,
+        passes: 2
       },
-      mangle: false
+      mangle: {
+        reserved: pureFuncs
+      }
     });
 
-    if (result1.error !== undefined) {
-      process.stderr.write(result1.error.message);
+    if (result.error !== undefined) {
+      process.stderr.write(result.error.message);
       process.exit(1);
     }
 
-    const result2 = UglifyJs.minify(result1.code, {
-      compress: false,
-      mangle: true
-    });
-
-    if (result2.error !== undefined) {
-      process.stderr.write(result2.error.message);
-      process.exit(1);
-    }
-
-    fs.writeFileSync(outputPath, result2.code);
+    fs.writeFileSync(outputPath, result.code);
     process.exit(0);
   }
 
