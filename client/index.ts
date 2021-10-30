6ee70e299aa12527b11d3da6fcc4d350bb5ce3de
Simon Lydell <simon.lydell@gmail.com>
2021-10-30 13:34:31 +0200

    Wip client

--- client/index.ts
+++ client/index.ts
@@ -4,6 +4,7 @@ const VERSION = "%VERSION%";
 const TARGET_NAME = "%TARGET_NAME%";
 const COMPILED_TIMESTAMP = "%COMPILED_TIMESTAMP%";
 const WEBSOCKET_PORT = "%WEBSOCKET_PORT%";
+const CONTAINER_ID = "elmWatch";
 
 WebSocketToClientMessage({
   VERSION,
@@ -11,3 +12,47 @@ WebSocketToClientMessage({
   COMPILED_TIMESTAMP,
   WEBSOCKET_PORT,
 });
+
+function run(): void {
+  const container = getOrCreateContainer();
+  const { shadowRoot } = container;
+
+  if (shadowRoot === null) {
+    throw new Error("TODO: No shadowRoot");
+  }
+
+  const existingTargetRoot = Array.from(shadowRoot.children).find(
+    (element) => element.getAttribute("data-target") === TARGET_NAME
+  );
+
+  if (existingTargetRoot !== undefined) {
+    return;
+  }
+
+  const targetRoot = document.createElement("div");
+  targetRoot.setAttribute("data-target", TARGET_NAME);
+  shadowRoot.append(targetRoot);
+
+  // time to do webSockets!
+}
+
+function getOrCreateContainer(): HTMLElement {
+  const existing = document.getElementById(CONTAINER_ID);
+
+  if (existing !== null) {
+    return existing;
+  }
+
+  const container = document.createElement("div");
+  container.id = CONTAINER_ID;
+  container.style.all = "unset";
+  container.style.position = "fixed";
+  container.style.zIndex = "2147483647"; // Maximum z-index supported by browsers.
+  container.style.left = "0";
+  container.style.bottom = "0";
+  container.attachShadow({ mode: "open" });
+  document.documentElement.append(container);
+  return container;
+}
+
+run();
