ff707a5f182d44a0fc4a159bcb491086acc67ca2
Simon Lydell <simon.lydell@gmail.com>
2022-10-25 19:24:22 +0200

    Update npm packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -149,6 +149,7 @@ module.exports = {
         "@typescript-eslint/no-unsafe-argument": error,
         "@typescript-eslint/no-unsafe-assignment": error,
         "@typescript-eslint/no-unsafe-call": error,
+        "@typescript-eslint/no-unsafe-declaration-merging": error,
         "@typescript-eslint/no-unsafe-member-access": error,
         "@typescript-eslint/no-unsafe-return": error,
         "@typescript-eslint/no-unused-expressions": error,
