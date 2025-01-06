lydell/elm-watch

commit fd6d5b0e295187bd1c1120af923bc61bf5f300c2

Add Node.js support with example

client/client.ts
@@ -18,11 +18,11 @@ import {
   WebSocketToServerMessage,
 } from "./WebSocketMessages";
 
-// Support Web Workers, where `window` does not exist.
+// Support environments, such as Web Workers, where `window` does not exist.
 const window = globalThis as unknown as Window;
 
 // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-const IS_WEB_WORKER = window.window === undefined;
+const HAS_WINDOW = window.window !== undefined;
 
 // These used to be separate properties on `window`, like
 // `window.__ELM_WATCH_MOCKED_TIMINGS`. It’s better to group them all together
@@ -53,6 +53,7 @@ declare global {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
   interface Window {
     Elm?: ElmExports;
+    ELM_WATCH_FULL_RELOAD?: () => void;
     __ELM_WATCH: __ELM_WATCH;
   }
 }
@@ -192,19 +193,24 @@ const DEFAULT_ELM_WATCH: __ELM_WATCH = {
         // Ignore failing to write to sessionStorage.
       }
     }
-    if (IS_WEB_WORKER) {
+    // Allow customizing how to “reload the page”, for Web Workers and Node.js
+    // (or in the browser if you really want to).
+    if (typeof window.ELM_WATCH_FULL_RELOAD === "function") {
+      window.ELM_WATCH_FULL_RELOAD();
+    } else if (HAS_WINDOW) {
+      window.location.reload();
+    } else {
       if (message !== undefined) {
         // eslint-disable-next-line no-console
         console.info(message);
       }
-      // eslint-disable-next-line no-console
-      console.error(
+      const why =
         message === undefined
-          ? "elm-watch: You need to reload the page! I seem to be running in a Web Worker, so I can’t do it for you."
-          : `elm-watch: You need to reload the page! I seem to be running in a Web Worker, so I couldn’t actually reload the page (see above).`,
-      );
-    } else {
-      window.location.reload();
+          ? "because a hot reload was not possible"
+          : "see above";
+      const info = `elm-watch: A full reload or restart of the program running your Elm code is needed (${why}). In a web browser page, I would have reloaded the page. You need to do this manually, or define a \`globalThis.ELM_WATCH_FULL_RELOAD\` function.`;
+      // eslint-disable-next-line no-console
+      console.error(info);
     }
   },
 
@@ -548,7 +554,7 @@ function run(): void {
     // Ignore failing to read or delete from sessionStorage.
   }
 
-  const elements = IS_WEB_WORKER ? undefined : getOrCreateTargetRoot();
+  const elements = HAS_WINDOW ? getOrCreateTargetRoot() : undefined;
   const browserUiPosition =
     elements === undefined
       ? ORIGINAL_BROWSER_UI_POSITION
@@ -943,59 +949,60 @@ const initMutable =
     // Firefox throws this error via `FocusedTab`:
     // DOMException: An attempt was made to use an object that is not, or is no longer, usable
     // So wait until the WebSocket is ready before starting those listeners.
-    mutable.webSocket.addEventListener(
-      "open",
-      () => {
-        removeListeners = [
-          addEventListener(window, "focus", (event) => {
-            // Used in tests to trigger focus for just one target.
-            if (event instanceof CustomEvent && event.detail !== TARGET_NAME) {
-              return;
-            }
-            dispatch({ tag: "FocusedTab" });
-          }),
-          addEventListener(window, "visibilitychange", () => {
-            if (document.visibilityState === "visible") {
-              dispatch({
-                tag: "PageVisibilityChangedToVisible",
-                date: getNow(),
-              });
-            }
-          }),
-          ...(elements === undefined
-            ? []
-            : [
-                addEventListener(
-                  elements.shadowRoot,
-                  BROWSER_UI_MOVED_EVENT,
-                  (event) => {
-                    dispatch({
-                      tag: "BrowserUiMoved",
-                      browserUiPosition: BrowserUiPositionWithFallback(
-                        (event as CustomEvent).detail,
-                      ),
-                    });
+    if (elements !== undefined) {
+      mutable.webSocket.addEventListener(
+        "open",
+        () => {
+          removeListeners = [
+            addEventListener(window, "focus", (event) => {
+              // Used in tests to trigger focus for just one target.
+              if (
+                event instanceof CustomEvent &&
+                event.detail !== TARGET_NAME
+              ) {
+                return;
+              }
+              dispatch({ tag: "FocusedTab" });
+            }),
+            addEventListener(window, "visibilitychange", () => {
+              if (document.visibilityState === "visible") {
+                dispatch({
+                  tag: "PageVisibilityChangedToVisible",
+                  date: getNow(),
+                });
+              }
+            }),
+            addEventListener(
+              elements.shadowRoot,
+              BROWSER_UI_MOVED_EVENT,
+              (event) => {
+                dispatch({
+                  tag: "BrowserUiMoved",
+                  browserUiPosition: BrowserUiPositionWithFallback(
+                    (event as CustomEvent).detail,
+                  ),
+                });
+              },
+            ),
+            addEventListener(
+              elements.shadowRoot,
+              CLOSE_ALL_ERROR_OVERLAYS_EVENT,
+              () => {
+                dispatch({
+                  tag: "UiMsg",
+                  date: getNow(),
+                  msg: {
+                    tag: "ChangedOpenErrorOverlay",
+                    openErrorOverlay: false,
                   },
-                ),
-                addEventListener(
-                  elements.shadowRoot,
-                  CLOSE_ALL_ERROR_OVERLAYS_EVENT,
-                  () => {
-                    dispatch({
-                      tag: "UiMsg",
-                      date: getNow(),
-                      msg: {
-                        tag: "ChangedOpenErrorOverlay",
-                        openErrorOverlay: false,
-                      },
-                    });
-                  },
-                ),
-              ]),
-        ];
-      },
-      { once: true },
-    );
+                });
+              },
+            ),
+          ];
+        },
+        { once: true },
+      );
+    }
 
     __ELM_WATCH.RELOAD_STATUSES.set(TARGET_NAME, {
       tag: "MightWantToReload",
@@ -1168,9 +1175,19 @@ function initWebSocket(
   elmCompiledTimestamp: number,
   dispatch: (msg: Msg) => void,
 ): WebSocket {
-  const hostname =
-    window.location.hostname === "" ? "localhost" : window.location.hostname;
-  const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+  const [hostname, protocol] =
+    // Browser: `window.location` always exists.
+    // Web Worker: `window` has been set to `globalThis` at the top, which has `.location`.
+    // Node.js: `window.location` does not exist.
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    window.location === undefined
+      ? ["localhost", "ws"]
+      : [
+          window.location.hostname === ""
+            ? "localhost"
+            : window.location.hostname,
+          window.location.protocol === "https:" ? "wss" : "ws",
+        ];
   const url = new URL(`${protocol}://${hostname}:${WEBSOCKET_PORT}/elm-watch`);
   url.searchParams.set("elmWatchVersion", VERSION);
   url.searchParams.set("targetName", TARGET_NAME);
@@ -1183,6 +1200,15 @@ function initWebSocket(
   });
 
   webSocket.addEventListener("close", () => {
+    dispatch({
+      tag: "WebSocketClosed",
+      date: getNow(),
+    });
+  });
+
+  // In Node.js, if you start the Node.js program (with the elm-watch client) before you start `elm-watch` hot,
+  // the "error" event is emitted. Treating it like "closed" allows us to try reconnecting.
+  webSocket.addEventListener("error", () => {
     dispatch({
       tag: "WebSocketClosed",
       date: getNow(),
@@ -1736,9 +1762,16 @@ const runCmd =
         if (elements === undefined) {
           if (model.status.tag !== model.previousStatusTag) {
             const isError = statusToStatusType(model.status.tag) === "Error";
+            const isWebWorker =
+              typeof (window as unknown as { WorkerGlobalScope: unknown })
+                .WorkerGlobalScope !== "undefined" && !isError;
+            const consoleMethodName =
+              // `console.info` looks nicer in the browser console for Web Workers.
+              // On Node.js, we want to always print to stderr.
+              isError || !isWebWorker ? "error" : "info";
             // eslint-disable-next-line no-console
-            const consoleMethod = isError ? console.error : console.info;
-            consoleMethod(renderWebWorker(model, info));
+            const consoleMethod = console[consoleMethodName];
+            consoleMethod(renderWithoutDomElements(model, info));
           }
         } else {
           const { targetRoot } = elements;
@@ -2118,7 +2151,7 @@ type Info = {
   errorOverlayVisible: boolean;
 };
 
-function renderWebWorker(model: Model, info: Info): string {
+function renderWithoutDomElements(model: Model, info: Info): string {
   const statusData = statusIconAndText(model);
   return `${statusData.icon} elm-watch: ${statusData.status} ${formatTime(
     model.status.date,
@@ -3711,8 +3744,10 @@ Maybe the JavaScript code running in the browser was compiled with an older vers
 
 void renderMockStatuses;
 
-// `Platform.worker` programs can also be run in Node.js. (`this === exports` there.)
-// But there’s no `WebSocket`. So don’t bother with starting anything.
+// Node.js used to not have a `WebSocket` global. We used to skip running the elm-watch client
+// for `Platform.worker` programs run (in a certain way) in Node.js.
+// These days, Node.js does have `WebSocket`, and the elm-watch client does work in Node.js.
+// But we keep the check for backwards compatibility.
 if (typeof WebSocket !== "undefined") {
   run();
 }

client/proxy.ts
@@ -1,5 +1,12 @@
-// Support Web Workers, where `window` does not exist.
+// Support environments, such as Web Workers, where `window` does not exist.
 const window = globalThis as unknown as Window;
+
+// This is replaced with `const __this__ = this;` in Inject.ts.
+// That’s a tooling workaround: This file appears to be a module (where global
+// `this` is `undefined` and often compiled to a literal `undefined`). But it
+// is actually used as a script in the end. Since we do a string replacement on
+// this file anyway for `%TARGET_NAME%`, this was an easy solution for `this` too.
+const __this__ = window;
 
 const error: Error & { elmWatchProxy?: true } = new Error(
   `
@@ -17,7 +24,7 @@ them first.
 
 error.elmWatchProxy = true;
 
-const existing = window.Elm;
+const existing = __this__.Elm;
 const existingObject =
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
   typeof existing === "object" && existing !== null ? existing : undefined;
@@ -49,7 +56,7 @@ const elmProxy = new Proxy(existingObject ?? {}, {
   },
 });
 
-window.Elm = elmProxy;
+__this__.Elm = elmProxy;
 
 window.__ELM_WATCH.REGISTER("%TARGET_NAME%", {});
 

example/nodejs-program.js
@@ -1,0 +1,84 @@
+import fs from "node:fs";
+import path from "node:path";
+import readline from "node:readline";
+
+async function run() {
+  globalThis.ELM_WATCH_FULL_RELOAD = restart;
+
+  const elmPath = path.join(
+    import.meta.dirname,
+    "public",
+    "build",
+    "NodeJS.js",
+  );
+
+  let app;
+
+  try {
+    const Elm = getElm(elmPath);
+    app = Elm.NodeJS.init();
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      // If the Elm JS file does not exist yet, wait for it to be created
+      // (when elm-watch is started).
+      fs.mkdirSync(path.dirname(elmPath), { recursive: true });
+      fs.watch(path.dirname(elmPath), (_, filename) => {
+        if (filename === path.basename(elmPath)) {
+          restart();
+        }
+      });
+      console.warn("Watching for file to be created:", elmPath);
+    } else {
+      // Other errors are most likely from the proxy file throwing about parts of
+      // `Elm` not being available yet. Then we want to wait for the elm-watch client
+      // to receive a message that it needs to do a full reload.
+      console.error(error.message);
+      console.warn("Waiting for changes…");
+    }
+    return;
+  }
+
+  app.ports.toJs.subscribe(([message, count]) => {
+    console.log(count, "|", message);
+  });
+
+  console.log("Write something and press Enter.");
+  for await (const line of readline.createInterface({ input: process.stdin })) {
+    app.ports.fromJs.send(line);
+  }
+}
+
+// This imports compiled Elm JS by reading the file and then eval-ing it.
+//
+// It is also possible import an Elm module like this:
+//
+// import { createRequire } from "module";
+// const require = createRequire(import.meta.url);
+// const { Elm } = require(elmPath);
+//
+// The downside is that `node --watch` will then reload the whole program
+// whenever the compiled Elm JS file changes, which means that hot reloading
+// is never used.
+function getElm(elmPath) {
+  const code = fs.readFileSync(elmPath, "utf-8");
+  const f = new Function(code);
+  const output = {};
+  f.call(output);
+  return output.Elm;
+}
+
+function restart() {
+  // Since this program is run with `node --watch`, touching this
+  // file causes the program to be restarted.
+  touch(import.meta.filename);
+}
+
+function touch(filePath) {
+  const now = new Date();
+  fs.utimesSync(filePath, now, now);
+}
+
+run().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});

example/postprocess.js
@@ -30,7 +30,10 @@ export default async function postprocess({
  * @returns {string}
  */
 function patch(targetName, code) {
-  if (targetName.includes("ESM")) {
+  if (targetName === "Node.js") {
+    // Disable Elm’s “Compiled in DEV mode” logs.
+    return code.replace(/^console\.warn\('Compiled in DEV mode\..+'\);$/m, "");
+  } else if (targetName.includes("ESM")) {
     // Turn the Elm JS into an ECMAScript module:
     return `const output = {}; (function(){${code}}).call(output); export default output.Elm;`;
   } else {

example/src/NodeJS.elm
@@ -1,0 +1,53 @@
+port module NodeJS exposing (main)
+
+
+port fromJs : (String -> msg) -> Sub msg
+
+
+port toJs : ( String, Int ) -> Cmd msg
+
+
+type Msg
+    = GotMessageFromJs String
+
+
+type alias Model =
+    { count : Int
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { count = 0
+      }
+    , Cmd.none
+      -- For testing adding a Cmd to init (should need a full reload):
+      -- toJs ( "Init message!", 1337 )
+    )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        GotMessageFromJs message ->
+            let
+                count =
+                    model.count + 1
+            in
+            ( { model | count = count }
+            , toJs ( String.toUpper message ++ "!", count )
+            )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    fromJs GotMessageFromJs
+
+
+main : Program () Model Msg
+main =
+    Platform.worker
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        }

src/Inject.ts
@@ -832,10 +832,12 @@ export function proxyFile(
     webSocketPort,
     debug,
   );
-  const proxyCodeString = ClientCode.proxy.replace(
-    /"%TARGET_NAME%"/g,
-    Codec.JSON.stringify(Codec.string, outputPath.targetName),
-  );
+  const proxyCodeString = ClientCode.proxy
+    .replace(
+      /"%TARGET_NAME%"/g,
+      Codec.JSON.stringify(Codec.string, outputPath.targetName),
+    )
+    .replace(/__this__ = window/g, "__this__ = this");
   // In ESM, importing something that isn’t exported is an error.
   // When ESM-ify:ing Elm’s output, the two most likely export names
   // are `default` and `Elm`, so make those available. To not break

commit 9210e2b96c6c544e2d69f5a7ebff64f346c802b0

Fix reconnect test
Regression from commit fd6d5b0e295187bd1c1120af923bc61bf5f300c2.

client/client.ts
@@ -1330,21 +1330,27 @@ function update(msg: Msg, model: Model): [Model, Array<Cmd>] {
     case "UiMsg":
       return onUiMsg(msg.date, msg.msg, model);
 
-    case "WebSocketClosed": {
-      const attemptNumber =
-        "attemptNumber" in model.status ? model.status.attemptNumber + 1 : 1;
-      return [
-        {
-          ...model,
-          status: {
-            tag: "SleepingBeforeReconnect",
-            date: msg.date,
-            attemptNumber,
+    case "WebSocketClosed":
+      // This message is triggered from both the WebSocket "error" and "closed" events.
+      // Sometimes, both events happen at the same time. We only want to start reconnecting once,
+      // so check if we’re already sleeping before doing anything.
+      if (model.status.tag === "SleepingBeforeReconnect") {
+        return [model, []];
+      } else {
+        const attemptNumber =
+          "attemptNumber" in model.status ? model.status.attemptNumber + 1 : 1;
+        return [
+          {
+            ...model,
+            status: {
+              tag: "SleepingBeforeReconnect",
+              date: msg.date,
+              attemptNumber,
+            },
           },
-        },
-        [{ tag: "SleepBeforeReconnect", attemptNumber }],
-      ];
-    }
+          [{ tag: "SleepBeforeReconnect", attemptNumber }],
+        ];
+      }
 
     case "WebSocketConnected":
       return [