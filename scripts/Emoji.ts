39efbe5cfec75ac74888b8e6bbf22b5b34e5fbee
Simon Lydell <simon.lydell@gmail.com>
2025-08-07 00:50:05 +0200

    Match more emoji

--- scripts/Emoji.ts
+++ scripts/Emoji.ts
@@ -9,7 +9,7 @@ const nonMatching: Array<string> = [];
 async function run(): Promise<void> {
   const response = await fetch(
     // Tested on this commit:
-    // https://raw.githubusercontent.com/mathiasbynens/emoji-test-regex-pattern/85a0059035a7650f46294647482b95d50e84ad22/dist/latest/index-strings.txt
+    // "https://raw.githubusercontent.com/mathiasbynens/emoji-test-regex-pattern/700ac302b80845324648a068738f9f809b7fce8c/dist/latest/index-strings.txt",
     "https://raw.githubusercontent.com/mathiasbynens/emoji-test-regex-pattern/main/dist/latest/index-strings.txt",
   );
 
--- src/EmojiRegex.ts
+++ src/EmojiRegex.ts
@@ -1,40 +1,17 @@
 /*
 
-Copied from: https://github.com/slevithan/emoji-regex-xs/blob/c107730f26757bed4faddc62f3aef336f521475f/index.mjs
-Modified to only match at the beginning of strings, and followed by a space.
+This regex is not perfect – see https://github.com/slevithan/emoji-regex-xs.
+But I think it’s good enough.
 
 See scripts/Emoji.ts for exactly which emojis do and do not match this regex.
 
 */
-/* @license
-
-MIT License
-
-Copyright (c) 2024 Steven Levithan
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-const r = String.raw;
-const seq = r`(?:\p{Emoji}\uFE0F\u20E3?|\p{Emoji_Modifier_Base}\p{Emoji_Modifier}?|\p{Emoji_Presentation})`;
-const sTags = r`\u{E0061}-\u{E007A}`;
-export const STARTS_WITH_EMOJI_REGEX = new RegExp(
-  r`^(?:[\u{1F1E6}-\u{1F1FF}]{2}|\u{1F3F4}[${sTags}]{2}[\u{E0030}-\u{E0039}${sTags}]{1,3}\u{E007F}|${seq}(?:\u200D${seq})*) `,
-  "u",
-);
+export const STARTS_WITH_EMOJI_REGEX = (() => {
+  try {
+    // eslint-disable-next-line prefer-regex-literals
+    return RegExp(`^[\\p{RGI_Emoji}\\p{Extended_Pictographic}]\uFE0F? `, "v");
+  } catch {
+    // If the `v` flag isn’t supported (Node.js < 20), return a regex that never matches.
+    return /.^/;
+  }
+})();
